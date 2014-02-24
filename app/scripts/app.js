1b720e22bc25fd143f081b87601a4bf5e11c6a85
frankselhorst <frank.selhorst@kimengi.com>
2014-02-24 21:44:12 +0100

    wipje

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,21 +7,22 @@ var app = angular.module('spaarnwoudeApp', [
   'ngRoute'
 ]);
 
- app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/activiteiten', {
-        templateUrl: 'views/activiteiten.html',
-        controller: 'ActiviteitenCtrl'
-      })
-      .when('/kaart', {
-        templateUrl: 'views/kaart.html',
-        controller: 'KaartCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/activiteiten', {
+      templateUrl: 'views/activiteiten.html',
+      controller: 'ActiviteitenCtrl'
+    })
+    .when('/kaart', {
+      templateUrl: 'views/kaart.html',
+      controller: 'KaartCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,23 +3,31 @@
 app.run(function($rootScope, activiteiten){
     activiteiten.success(function(data) { 
         $rootScope.activiteiten = data;
-    });
+    })
 
-});
+    $rootScope.activiteit = {
+        naam:'undefined',
+    }
+
+})
 
 app.factory('activiteiten', function($http) { 
     return $http.get('../data/activiteiten.json');
-});
+})
 
 app.factory('weerData', function($http) { 
     return $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Spaarndam&mode=json&units=metric&cnt=5');
-});
+})
 
-app.controller('HomeCtrl', function ($scope, activiteiten, weerData) {
+app.controller('AppCtrl', function($scope, $rootScope, weerData) {
+    $rootScope.activiteit = {};
+})
+
+app.controller('HomeCtrl', function ($scope, $rootScope, weerData) {
     $scope.weer = {};  
     weerData.success(function(data) { 
         $scope.getWeather(data);
-    });
+    })
 
     $scope.getWeather = function getWeather(weather) {
         $scope.weer.vandaag = weather.list[0];
@@ -28,15 +36,19 @@ app.controller('HomeCtrl', function ($scope, activiteiten, weerData) {
         $scope.weer.morgen.dag = 'Morgen';
         $scope.weer.overmorgen = weather.list[2];
         $scope.weer.overmorgen.dag = 'Overmorgen';
-        // console.log($scope.weer.vandaag);
     }
-});
+})
 
-app.controller('ActiviteitenCtrl', function ($scope, activiteiten) {
+app.controller('ActiviteitenCtrl', function ($scope, $rootScope) {
+    $scope.getActivity = function(id) {
+        var activiteit = _.filter($rootScope.activiteiten, function(a) {
+           return a.id === id;
+        })
 
-});
+        $rootScope.activiteit = activiteit;
+    }
+})
 
-app.controller('KaartCtrl', function ($scope, activiteiten) {
+app.controller('KaartCtrl', function ($scope) {
 
-
-});
+})
