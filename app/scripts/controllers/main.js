3caad2ebf7362858335094952663078b7501ba7c
frankselhorst <frank.selhorst@kimengi.com>
2014-02-24 22:31:22 +0100

    split controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,32 +23,3 @@ app.controller('AppCtrl', function($scope, $rootScope, weerData) {
     $rootScope.activiteit = {};
 })
 
-app.controller('HomeCtrl', function ($scope, $rootScope, weerData) {
-    $scope.weer = {};  
-    weerData.success(function(data) { 
-        $scope.getWeather(data);
-    })
-
-    $scope.getWeather = function getWeather(weather) {
-        $scope.weer.vandaag = weather.list[0];
-        $scope.weer.vandaag.dag = 'Vandaag';
-        $scope.weer.morgen = weather.list[1];
-        $scope.weer.morgen.dag = 'Morgen';
-        $scope.weer.overmorgen = weather.list[2];
-        $scope.weer.overmorgen.dag = 'Overmorgen';
-    }
-})
-
-app.controller('ActiviteitenCtrl', function ($scope, $rootScope) {
-    $scope.getActivity = function(id) {
-        var activiteit = _.filter($rootScope.activiteiten, function(a) {
-           return a.id === id;
-        })
-
-        $rootScope.activiteit = activiteit;
-    }
-})
-
-app.controller('KaartCtrl', function ($scope) {
-
-})
