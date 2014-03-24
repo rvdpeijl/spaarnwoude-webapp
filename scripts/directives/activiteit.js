c1277fbf4e9011d2c89d4046151b650323c7e13d
frankselhorst <frank.selhorst@kimengi.com>
2014-03-24 22:50:47 +0100

    changes

--- scripts/directives/activiteit.js
+++ scripts/directives/activiteit.js
@@ -3,6 +3,21 @@ app.directive('activiteit', function($rootScope) {
         restrict: 'E',
         link: function($scope, element, attrs) {
 
+        var colors = {
+            beleven: {
+                hex: '#dbdb01'
+            },
+            doen: {
+                hex: '#dbdb01'
+            },
+            genieten: {
+                hex: '#b7037e'
+            },
+            verblijven: {
+                hex: '#32aadf'
+            }
+        };
+
             var id = $scope.$eval(attrs.activiteitId),
                 activiteiten = $scope.activiteiten,
                 activiteit = $scope.singleHandler.filter(id);
@@ -26,7 +41,7 @@ app.directive('activiteit', function($rootScope) {
             wrapper.appendChild(titel);
             wrapper.appendChild(knop);
             knop.innerHTML = 'Bekijk';
-            
+            knop.style.backgroundColor = colors[activiteit.categorie].hex;
             knop.onclick = function() {
                 $scope.singleHandler.showSingle(id);
             };
