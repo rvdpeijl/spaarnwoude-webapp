80dbae3af0b2d255455978b4e2cb445ea0c93145
Robbin van der Peijl <robbin@noprotocol.com>
2014-04-08 21:49:32 +0200

    kleuren agenda

--- scripts/controllers/agenda.js
+++ scripts/controllers/agenda.js
@@ -2,4 +2,23 @@
 
 app.controller('AgendaCtrl', function ($scope, $rootScope, $routeParams) {
     $rootScope.kaartLoaded = 0;
+
+    $scope.getAgendaColor = function(categorie) {
+      var colors = {
+              beleven: {
+                  hex: '#f18a01'
+              },
+              doen: {
+                  hex: '#b7037e'
+              },
+              genieten: {
+                  hex: '#32aadf'
+              },
+              verblijven: {
+                  hex: '#dbdb00'
+              }
+          };
+
+      return colors[categorie].hex;
+    }
 });
