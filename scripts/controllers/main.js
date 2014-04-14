30c95753b86eb88fa342dd912cb5b1c43e01fd39
Robbin van der Peijl <robbin@noprotocol.com>
2014-04-14 21:46:38 +0200

    merge

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -56,3 +56,29 @@ app.controller('FbCtrl', function($scope, $rootScope) {
         FB.login();
     }
 })
+
+app.animation('.fade', function() {
+  return {
+    beforeAddClass : function(element, className, done) {
+      if(className == 'ng-hide') {
+        jQuery(element).animate({
+          opacity:0
+        }, done);
+      }
+      else {
+        done();
+      }
+    },
+    removeClass : function(element, className, done) {
+      if(className == 'ng-hide') {
+        element.css('opacity',0);
+        jQuery(element).animate({
+          opacity:1
+        }, done);
+      }
+      else {
+        done();
+      }
+    }
+  };
+});
