ea562e29d5fb6da57f8d15fc1eef262086dfa2c1
Robbin van der Peijl <robbin@rvdpeijl.nl>
2015-01-24 19:47:14 +0100

    opzetje map

--- public/Gulpfile.js
+++ public/Gulpfile.js
@@ -2,14 +2,8 @@ var gulp        = require('gulp'),
     sass        = require('gulp-ruby-sass')
 	sourcemaps = require('gulp-sourcemaps');
 
-// gulp.task('styles', function() {
-//     return gulp.src('styles/sass/main.scss')
-//     .pipe(sass({ style: 'expanded', }))
-//     .pipe(gulp.dest('styles/css'));
-// });
-
 gulp.task('sass', function() {
-    return sass('styles/sass/main.scss', { sourcemap: true })
+    return sass('styles/sass/', { sourcemap: true })
     .on('error', function (err) {
       console.error('Error', err.message);
    })
@@ -26,10 +20,5 @@ gulp.task('watch', function() {
     gulp.watch(['styles/sass/main.scss',
     'styles/sass/**/*.scss'], ['sass']);
 });
-
-// gulp.task('watch', function() {
-//     gulp.watch(['styles/sass/main.scss',
-//     'styles/sass/**/*.scss'], ['styles']);
-// });
 
 gulp.task('default', ['sass', 'watch']);
--- public/app/modules/map/map.controller.js
+++ public/app/modules/map/map.controller.js
@@ -6,16 +6,32 @@
         .controller('Map', Map);
 
     /* @ngInject */
-    Map.$inject = ['activities']; // activities come from resolve
+    Map.$inject = ['activities', '$window', '$rootScope']; // activities come from resolve
 
-    function Map(activities) {
+    function Map(activities, $window, $rootScope) {
         /*jshint validthis: true */
         var vm = this;
         vm.title = 'Map';
         vm.activities = activities;
+        vm.infoWindowVisible = false;
+
+        var tooltip = document.getElementById('tooltip');
+        var infoWindow = document.getElementById('infoWindow');
+        var infoWindowName = document.getElementById('infoWindowName');
+        var infoWindowDescription = document.getElementById('infoWindowDescription');
+
         vm.map = { 
             center: { latitude: 52.413307, longitude: 4.680558 }, 
-            zoom: 13 
+            zoom: 13,
+            options: {
+                scrollwheel: false
+            },
+            events: {
+                dragstart: function() {
+                    infoWindow.style.opacity = 0;
+                    vm.infoWindowVisible = false;
+                }
+            }
         };
 
         vm.marker = {
@@ -24,14 +40,33 @@
                 longitude: 4.680558
             },
             icon: '/img/icons/blue_location.png',
-            options: { name: 'Marky Marker' },
+            options: { name: 'Marky Markskizzle', description: 'Dit is descripzur' },
             events:  { 
                 mouseover: function(gMarker, eventName, model) {
-                    vm.marker.show = true;
-                    console.log(eventName);
+                    if (!vm.infoWindowVisible) {
+                        var left = ($window.event.clientX) + 'px';
+                        var top = ($window.event.clientY-50) + 'px';
+                        tooltip.style.opacity = 100;
+                        tooltip.style.left = left;
+                        tooltip.style.top = top;
+                        tooltip.innerHTML = gMarker.name;
+                    };
                 },
                 mouseout: function(gMarker, eventName, model) {
-                    vm.marker.show = false;
+                    var tooltip = document.getElementById('tooltip');
+                    tooltip.style.opacity = 0;
+                },
+                click: function(gMarker, eventName, model) {
+                    var left = ($window.event.clientX) + 'px';
+                    var top = ($window.event.clientY-50) + 'px';
+
+                    vm.infoWindowVisible = true;
+                    tooltip.style.opacity = 0;
+                    infoWindow.style.opacity = 100;
+                    infoWindow.style.left = left;
+                    infoWindow.style.top = top;
+                    infoWindowDescription.innerHTML = gMarker.description
+                    infoWindowName.innerHTML = gMarker.name;
                 }
             },
             show: false
