5e3cbea7767603e85c65c07ad98ae250db51e4c2
Frank Selhorst <frankselhorst@frank.local>
2015-01-24 16:02:00 +0100

    fixed Gulpfile sass task

--- public/Gulpfile.js
+++ public/Gulpfile.js
@@ -1,15 +1,35 @@
 var gulp        = require('gulp'),
-    sass        = require('gulp-ruby-sass');
+    sass        = require('gulp-ruby-sass')
+	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', function() {
-    return gulp.src('styles/sass/main.scss')
-    .pipe(sass({ style: 'expanded', }))
+// gulp.task('styles', function() {
+//     return gulp.src('styles/sass/main.scss')
+//     .pipe(sass({ style: 'expanded', }))
+//     .pipe(gulp.dest('styles/css'));
+// });
+
+gulp.task('sass', function() {
+    return sass('styles/sass/', { sourcemap: true })
+    .on('error', function (err) {
+      console.error('Error', err.message);
+   })
+
+    .pipe(sourcemaps.write('maps', {
+        includeContent: false,
+        sourceRoot: 'sass'
+    }))
+
     .pipe(gulp.dest('styles/css'));
 });
 
 gulp.task('watch', function() {
     gulp.watch(['styles/sass/main.scss',
-    'styles/sass/**/*.scss'], ['styles']);
+    'styles/sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['styles', 'watch']);
+// gulp.task('watch', function() {
+//     gulp.watch(['styles/sass/main.scss',
+//     'styles/sass/**/*.scss'], ['styles']);
+// });
+
+gulp.task('default', ['sass', 'watch']);
