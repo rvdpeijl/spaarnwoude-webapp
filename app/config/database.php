c5838ef45178c02379b05fc82ac684656f7880e0
Frank Selhorst <frankselhorst@frank.local>
2015-02-01 16:05:43 +0100

    modal styling

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'spaarnwoude',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'Abc023',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
